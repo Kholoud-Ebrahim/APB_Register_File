df21debb35c66995d65ca7f0c00df8a154ac759e
Kholoud Ebrahim <108447715+Kholoud-Ebrahim@users.noreply.github.com>
2023-11-04 02:01:52 +0200

    Delete 1. Implicit Predictor directory

--- 1. Implicit Predictor/tb/apb_rf_intf.sv
+++
@@ -1,8 +0,0 @@
-interface apb_rf_intf #(parameter SIZE = 32) (input pclk, presetn);
-    logic [SIZE-1:0]   paddr;
-    logic [SIZE-1:0]   pwdata;
-    logic              psel;
-    logic              pwrite;
-    logic              penable;
-    logic [SIZE-1:0]   prdata;
-endinterface :apb_rf_intf
--- 1. Implicit Predictor/tb/param_pkg.sv
+++
@@ -1,8 +0,0 @@
-package param_pkg;
-    import uvm_pkg::*;
-    `include "uvm_macros.svh"
-
-    parameter PERIOD = 20;
-    parameter SIZE   = 32;
-
-endpackage :param_pkg
